637c1c2ac67ced6ed2e3179cd40674281b254fc6
shubham-ai99 <79351435+shubham-ai99@users.noreply.github.com>
2024-04-29 14:22:33 +0530

    Add files via upload

--- JavaCodes/src/Main.java
+++ JavaCodes/src/Main.java
@@ -7,26 +7,11 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 5; i++) {
-
+        for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
-
-
-            int a=20;
-            Integer i=Integer.valueOf(a);//converting int into Integer explicitly
-            Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally
-
-            System.out.println(a+" "+i+" "+j);
-
-        System.out.println("***********************************************************************************");
-
-            int b=5;
-        System.out.println(b);
-
-        String name="shubz";
-        System.out.println(name);
+            System.out.println("i = " + i);
         }
     }
+}
