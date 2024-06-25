3dfa1a92beccbab179f6ea69ff8dda7419015dfd
shubham-ai99 <gawades963@gmail.com>
2024-06-25 14:36:48 +0530

    Re-Commit All files

--- JavaCodes/src/Main.java
+++ JavaCodes/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Welcome To Java Codes ");
+
+        for(int i = 0; i<1; i++) {
+            System.out.println("Hello");
+            break;
+        }
+
+        /*Calculator obj = new Calculator();
+        obj.calc(2);
+        obj.printNum();*/
         }
     }
 
